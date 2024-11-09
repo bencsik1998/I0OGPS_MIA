1daedd1d8bd40ef22ccd0749227e187d5b160102
Peter <100600984+bencsik1998@users.noreply.github.com>
2024-11-09 13:55:55 +0100

    Update calculator.py
    Implementing power, sqrt and modulo calculations.

--- calculator.py
+++ calculator.py
@@ -12,3 +12,14 @@ class Calculator:
         if y == 0:
             raise ValueError("Cannot divide by zero")
         return x / y
+
+     def power(self, x, y):
+        return x ** y
+
+    def square_root(self, x):
+        if x < 0:
+            raise ValueError("Nem lehet negatív számból négyzetgyököt vonni")
+        return x ** 0.5
+
+    def modulo(self, x, y):
+        return x % y
